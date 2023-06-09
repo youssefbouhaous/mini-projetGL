e7ebd10bc447a7a75480ee8e085d4c66b4855ade
youssefbouhaous <120169413+youssefbouhaous@users.noreply.github.com>
2023-06-09 01:16:05 +0100

    Add files via upload

---
+++ model/utilisateur.php
@@ -0,0 +1,54 @@
+<?php
+class Utilisateur
+{
+    private $email, $nom, $password, $prenom;
+
+    public function __construct($email, $nom, $password, $prenom)
+    {
+        $this->email = $email;
+        $this->nom = $nom;
+        $this->password = $password;
+        $this->prenom = $prenom;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+}
+?>
